db2a39ca41fbab41a7b126e637965309c6e2bdc4
Juan Aldana <jufaldanabo@unal.edu.co>
2022-07-26 08:26:10 -0500

    upupdate requirements.txt

--- functions.py
+++ functions.py
@@ -1,2 +1,28 @@
+import pandas as pd
+
+
 def add_two_numbers(a: int, b: int) -> int:
     return a + b
+
+
+################################# ciencia de datos
+### 111111 222222 3333 444444 555555
+
+
+class Cleaning(object):
+    def __init__(self, df: pd.DataFrame, variable: str):
+        self.data = df
+        self.id = variable
+
+    def remmove_invalid_id(self) -> pd.DataFrame:
+        df_cleaning = self.data.drop_duplicates().copy()
+        id_remover = [int(str(x) * y) for x in range(10) for y in range(4, 9)]
+        df_final = df_cleaning[~df_cleaning[self.id].isin(id_remover)]
+        return df_final
+
+
+clean = Cleaning(
+    df=pd.DataFrame({"identificacion": [1, 2, 3, 4, 5555555, 66666]}),
+    variable="identificacion",
+)
+clean.remmove_invalid_id()
--- test/test_functions.py
+++ test/test_functions.py
@@ -1,5 +1,20 @@
-from functions import add_two_numbers
+from functions import add_two_numbers, Cleaning
+import pandas as pd
+import pytest
 
 
 def test_add_two_numbers():
-    assert (add_two_numbers(2, 6)) == 8
+    assert (add_two_numbers(1, 2)) == 6
+    assert (add_two_numbers(-1, 2)) == 1
+
+
+@pytest.fixture()
+def data_raw():
+    return Cleaning(
+        df=pd.DataFrame({"identificacion": [1, 2, 3, 4, 5555555, 66666]}),
+        variable="identificacion",
+    )
+
+
+def test_remmove_invalid_id(data_raw):
+    assert data_raw.remmove_invalid_id().shape == (4, 1)
